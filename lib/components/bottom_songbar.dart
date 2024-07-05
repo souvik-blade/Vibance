e751788beda1331527d62fc2c787bb5b3f4a73de
Sourav Prasad <souravprasad1869@gmail.com>
2024-07-05 22:02:42 +0530

    Changes Made
    Hero, Animated Container

--- lib/components/bottom_songbar.dart
+++ lib/components/bottom_songbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vibance/models/playlist_provider.dart';
 import 'package:vibance/models/song.dart';
+import 'package:vibance/pages/song_page.dart';
 
 class BottomSongBar extends StatelessWidget {
   @override
@@ -17,134 +18,80 @@ class BottomSongBar extends StatelessWidget {
           return const SizedBox();
         }
 
-        return value.currentDuration > Duration.zero
-            ? value.isPlaying
-                ? AnimatedContainer(
-                    padding: const EdgeInsets.all(10),
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                    width: MediaQuery.of(context).size.width,
-                    height: 92,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    duration: const Duration(milliseconds: 500),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
+        return (value.currentDuration > Duration.zero || value.isPlaying)
+            ? GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SongPage()),
+                  );
+                },
+                child: AnimatedContainer(
+                  padding: const EdgeInsets.all(12),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                  width: MediaQuery.of(context).size.width,
+                  height: 92,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  duration: const Duration(milliseconds: 500),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Hero(
+                              tag: playlist[currentIndex].songImagePath,
                               child: Image.asset(
                                 playlist[currentIndex].songImagePath,
                                 fit: BoxFit.cover,
-                                width: 60,
-                                height: 60,
+                                // width: 60,
+                                // height: 60,
                               ),
                             ),
-                            const SizedBox(width: 16),
-                            Text(
-                              playlist[currentIndex].songName,
-                              style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width / 30,
-                                color: Colors.white,
-                              ),
+                          ),
+                          const SizedBox(width: 16),
+                          Text(
+                            playlist[currentIndex].songName,
+                            style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 30,
+                              color: Colors.white,
                             ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                value.playPreviousSong();
-                              },
-                              icon: const Icon(Icons.skip_previous_rounded),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                value.pauseOrResume();
-                              },
-                              icon: value.isPlaying
-                                  ? const Icon(Icons.pause_rounded)
-                                  : const Icon(Icons.play_arrow_rounded),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                value.playNextSong();
-                              },
-                              icon: const Icon(Icons.skip_next_rounded),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
-                : AnimatedContainer(
-                    padding: const EdgeInsets.all(10),
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                    width: MediaQuery.of(context).size.width,
-                    height: 92,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    duration: const Duration(milliseconds: 500),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: Image.asset(
-                                playlist[currentIndex].songImagePath,
-                                fit: BoxFit.cover,
-                                width: 60,
-                                height: 60,
-                              ),
-                            ),
-                            const SizedBox(width: 16),
-                            Text(
-                              playlist[currentIndex].songName,
-                              style: TextStyle(
-                                fontSize:
-                                    MediaQuery.of(context).size.width / 30,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                value.playPreviousSong();
-                              },
-                              icon: const Icon(Icons.skip_previous_rounded),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                value.pauseOrResume();
-                              },
-                              icon: value.isPlaying
-                                  ? const Icon(Icons.pause_rounded)
-                                  : const Icon(Icons.play_arrow_rounded),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                value.playNextSong();
-                              },
-                              icon: const Icon(Icons.skip_next_rounded),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              value.playPreviousSong();
+                            },
+                            icon: const Icon(Icons.skip_previous_rounded),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              value.pauseOrResume();
+                            },
+                            icon: value.isPlaying
+                                ? const Icon(Icons.pause_rounded)
+                                : const Icon(Icons.play_arrow_rounded),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              value.playNextSong();
+                            },
+                            icon: const Icon(Icons.skip_next_rounded),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
             : const SizedBox();
       },
     );
